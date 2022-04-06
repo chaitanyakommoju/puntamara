ce059edc8ba84fe8947b593b35c37b228435d7b7
nu11secur1ty <venvaropt@gmail.com>
2022-04-06 09:14:17 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -17,7 +17,13 @@ cd puntamara/
 	sleep 3;
 cd thehive/
 	docker-compose up -d
+	cd ..
+	sleep 3;
+cd nextcloud/
+	docker-compose up -d
+	cd ..
 	sleep 3;
 	echo "Please wait ...";
+	echo "The installation is finished."
 		netstat -tulpn
 		exit 0;
