556fa099169c322158c9608fd93a1b0614cf8efd
nu11secur1ty <venvaropt@gmail.com>
2022-04-06 11:54:01 +0300

    Update docker-stopper.pl

--- docker-stopper.pl
+++ docker-stopper.pl
@@ -2,4 +2,5 @@
 my $ENV = `docker stop nextcloud_app_1 nextcloud_db_1 portainer thehive_cortex_1 thehive_elasticsearch_1 thehive_thehive_1`;
 print "Your status of your docker container is...\n";
 my $STAT = `docker images -a`;
+print "$STAT";
   exit 0;
