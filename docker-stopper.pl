97f5a9a1aaa9f81b55c9c86b34c72ce5144566c4
nu11secur1ty <venvaropt@gmail.com>
2022-04-06 11:56:12 +0300

    Update docker-stopper.pl

--- docker-stopper.pl
+++ docker-stopper.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 my $ENV = `docker stop nextcloud_app_1 nextcloud_db_1 portainer thehive_cortex_1 thehive_elasticsearch_1 thehive_thehive_1`;
-print "Your status of your docker container is...\n";
+print "Your containers are stoped...\n";
+sleep 3;
+print "Your available images is...\n";
 my $STAT = `docker images -a`;
 print "$STAT";
   exit 0;
