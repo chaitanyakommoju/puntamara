20899ccf9560445c73c36e0d54b40ebc6b3e88ef
nu11secur1ty <venvaropt@gmail.com>
2022-04-20 14:13:37 +0300

    Update docker-killer.pl

--- docker-killer.pl
+++ docker-killer.pl
@@ -1,12 +1,4 @@
-#!/usr/bin/perl
-use warnings;
-use strict;
-use diagnostics;
-
-my $cache = `docker system prune -f`;
-  print("$cache\n");
-  print("Are you sure you want to continue? [y/N]\n");
-my $all = `docker system prune -a`;
-  
-  print("$all\n");
+#!/usr/bin/bash
+docker system prune -f
+docker system prune -a
   exit 0;
